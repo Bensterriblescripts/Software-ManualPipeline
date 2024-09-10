fce1041bd4a2cac21bcb2a7afc6e659364aae76a
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-09-10 19:01:31 +1200

    begun, added some cli stuff for automation of install

---
+++ ManualPipeline/PowerAppsPipeline.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Text.RegularExpressions;
+namespace ManualPipeline;
+
+public abstract class PowerAppsPipeline() {
+    private static readonly Regex re_PowerAppsCLIPath = new(@".*\\AppData\\Local\\Microsoft\\PowerAppsCLI\\.*", RegexOptions.IgnoreCase);
+
+    /* Installation */
+    public static void InstallAll(ref String? error) {
+        if (!Generic.CheckResponseBool("PowerApps CLI is not installed. Install now? (y/n)")) {
+            error = "Exiting...";
+            return;
+        }
+        
+        // CLI
+        Console.WriteLine("\nInstalling the PowerApps CLI...");
+        if ((error = InstallCLI()) is not null) {
+            return;
+        }
+        Console.WriteLine("Installed the PowerApps CLI.");
+        Console.WriteLine("\nInstallation completed successfully.");
+    }
+    
+    private static String? InstallCLI() {
+        if (!Generic.CMDCommandAutoClose("winget install Microsoft.PowerAppsCLI")) {
+            return "\nPowerAppsCLI did not install correctly.\n\nCheck if winget is installed or has been disabled and check that the path has been added.\nInstall it manually from the Microsoft website here: https://aka.ms/PowerAppsCLI";
+        }
+        return null;
+    }
+    private static String? InstallPRT() {
+        if (!Generic.CMDCommandKeepOpen("pac tool prt")) {
+            return "\nPowerApps Plugin Registration Tool did not install correctly.\n\nInstall it manually by running pac tool prt.";
+        }
+        return null;
+    }
+    private static String? CreateAuth(String envName) {
+        if (!Generic.CMDCommandKeepOpen($"pac auth create --environment {envName} --name {envName}")) {
+            return $"\nFailed to create an authentication profile for {envName}.\n\nAdd one manually with: pac auth create --environment {envName} --name {envName}.";
+        }
+        return null;
+    }
+    
+    /* Startup */
+    public static Boolean Installed() {
+        IDictionary envs = Environment.GetEnvironmentVariables();
+        foreach (DictionaryEntry entry in envs) {
+            String? key = entry.Key?.ToString();
+            String? value = entry.Value?.ToString();
+            if (String.IsNullOrEmpty(key) || String.IsNullOrEmpty(value)) {
+                continue;
+            }
+            if (key.Contains("PATH", StringComparison.OrdinalIgnoreCase)) {
+                if (re_PowerAppsCLIPath.IsMatch(value)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    public static String? UpdateCLI() {
+        if (!Generic.CMDCommandAutoClose("pac install latest")) {
+            return "\nPowerAppsCLI update failed with an error.";
+        }
+        return null;
+    }
+    
+    /* Connecting */
+    public static String SelectEnvironment() {
+        Console.WriteLine("\nEnvironment\nDev: 1) MITOUAT 2) MITO UAT\nProd: 3) idk");
+        Int16 response = Generic.CheckResponseNumbered(ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3);
+        String? currentEnvironment = String.Empty;
+        if (response == 1) {
+            currentEnvironment = "MITOUAT";
+        }
+        else if (response == 2) {
+            currentEnvironment = "MITO UAT";
+        }
+        else if (response == 3) {
+            currentEnvironment = "MYMITONZ";
+        }
+        return currentEnvironment;
+    }
+    public static String? SelectAuth(String envName) {
+        if (!Generic.CMDCommandReturnOutput($"pac auth select --name {envName}")) {
+            return "\nFailed to select an authentication profile for this environment.\nCreating one...";
+        }
+        return null;
+    }
+}
---
+++ ManualPipeline/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ManualPipeline;
+
+public static class Program {
+    public static void Main(string[] args) {
+        String? error = null;
+        String env;
+        
+        /* PowerApps Portal CLI*/
+        if (!PowerAppsPipeline.Installed()) {
+            PowerAppsPipeline.InstallAll(ref error);
+            if (error is not null) {
+                Console.WriteLine(error);
+                return;
+            }
+            Console.WriteLine("This terminal window will need to be restarted for the environment variables to be available.\nPress any key to exit...");
+            _ = Console.ReadKey(true).Key;
+            return;
+        }
+        Console.WriteLine("Found PowerApps CLI.");
+        if ((error = PowerAppsPipeline.UpdateCLI()) is not null) {
+            Console.WriteLine(error);
+            return;
+        }
+        env = PowerAppsPipeline.SelectEnvironment();
+        if ((error = PowerAppsPipeline.SelectAuth(env)) is not null) {
+            Console.WriteLine(error);
+            return;
+        }
+        Console.WriteLine($"Environment set to {env}.");
+        
+        
+
+        if (error is not null) {
+            Console.WriteLine($"Uncaught error:\n{error}");
+        }
+        Console.WriteLine("Ended successfully.");
+    }
+}
